4a29c5f10697ed276261bf045d97200bcf433760
EN <109923311+CardboardDog@users.noreply.github.com>
2025-03-12 13:26:34 -0400

    wifi connections

--- loader/gui.py
+++ loader/gui.py
@@ -6,6 +6,9 @@ class Gui:
     Open = True
     Input = 0
     Select = False
+    Back = False
+    Keyboard = ''
+    Delete = False
     def __init__(self):
         self.Open = True
         self.Window = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
@@ -19,16 +22,25 @@ class Gui:
         pygame.display.flip()
         self.Input = 0 
         self.Select = False
+        self.Back = False
+        self.Keyboard = ""
+        self.Delete = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.Open = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     self.Input = -1
-                if event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN:
                     self.Input = 1
-                if event.key == pygame.K_RETURN:
+                elif event.key == pygame.K_RETURN:
                     self.Select = True
+                elif event.key == pygame.K_ESCAPE:
+                    self.Back = True
+                elif event.key == pygame.K_BACKSPACE:
+                    self.Delete = True
+                else:
+                    self.Keyboard += event.unicode
     def DrawText(self,text,pos,act):
         surf = self.Font.render(text,True,((0,0,0) if act else (255,255,255)))
         newpos = (pos[0],pos[1]*self.Size)
@@ -38,6 +50,20 @@ class Gui:
             bg.y = newpos[1]
             pygame.draw.rect(self.Window,(255,255,255),bg)
         self.Window.blit(surf,newpos)
+    def DrawEntry(self,text,pos,act):
+        surf = self.Font.render(text,True,((0,0,0) if act else (255,255,255)))
+        newpos = (pos[0],pos[1]*self.Size)
+        if(act):
+            bg = surf.get_rect()
+            bg.x = newpos[0]
+            bg.y = newpos[1]
+            pygame.draw.rect(self.Window,(255,255,255),bg)
+        self.Window.blit(surf,newpos)
+        if(act):
+            if(self.Delete):
+                return text[:-1]
+            return text+self.Keyboard
+        return text
 def Quit():
     pygame.quit()
 
--- loader/wifi.py
+++ loader/wifi.py
@@ -1,3 +1,95 @@
 import gui
+import os
+import re
+scanned = False
+homedir = os.getenv("HOME")+"/.cider"
+networks = []
+password = "example"
+mode = 0
+select = 0
+target = "invalid network"
+tested = False
+connected = 0
+def scan():
+    global networks
+    global scanned
+    if not scanned:
+        try:
+            os.remove(homedir+"/wifi.txt")
+        except:
+            pass
+        os.system("nmcli d wifi &> "+homedir+"/wifi.txt")
+        readnet = open(homedir+"/wifi.txt","r")
+        rawnet = readnet.read()
+        readnet.close()
+        rawnet = rawnet.replace("\n","")
+        rawnet = rawnet.split()
+        networks = []
+        net = -1
+        for word in rawnet:
+            if(word == "Infra"): # infrastructer mode
+                name = ""
+                wrd = net
+                itr = 0
+                while True:
+                    if re.search("..:..:..:..:..:..",rawnet[wrd]): # mac address as regex
+                                 break
+                    name = rawnet[wrd]+" "+name if itr!=0 else rawnet[wrd]
+                    wrd-=1
+                    itr += 1
+                networks.append(name)
+            net += 1
+        scanned = True
+        print("found networks: "+str(networks))
+def netpick(win):
+    global select
+    global mode
+    global target
+    off = 0
+    select += win.Input
+    select = select%len(networks)
+    for net in networks:
+        win.DrawText(net,(1,off),off==select)
+        off+=1
+    if win.Select:
+        target = networks[select]
+        mode+=1
+def connect(win):
+    global password
+    global mode
+    global tested
+    win.DrawText("password for "+target+":",(1,0),False)
+    win.DrawText("[enter to continue]",(1,2),False)
+    password = win.DrawEntry(password,(1,1),True)
+    if win.Select:
+        os.system("nmcli d wifi connect "+target+" password "+password)
+        tested = False
+        mode+=1
+def feedback(win):
+    global tested
+    global mode
+    global connected
+    if not tested:
+        connected = os.system("ping -c 4 1.1.1.1")
+        tested = True
+    if connected == 0:
+        win.DrawText("Connected",(1,0),False)
+    else:
+        win.DrawText("Failed to connect :(",(1,0),False)
+    win.DrawText("[enter to continue] [escape to retry]",(1,1),True)
+    if(win.Select):
+        mode = -1
+modes = [netpick,connect,feedback]
 def tick(win):
-    return 1
+    global scanned
+    global networks
+    global mode
+    scan()
+    modes[mode](win)
+    if win.Back:
+        mode-=1
+    if mode < 0:
+        scanned = False
+        mode = 0
+        return 0
+    return 4
